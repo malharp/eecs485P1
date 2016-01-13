fee09d1e5301c75773e0568b7a25193cedb18940
vagrant <vagrant@precise32.(none)>
2016-01-13 02:35:07 +0000

    hope

---
+++ controllers/main.py
@@ -0,0 +1,16 @@
+from flask import *
+from extensions import mysql
+
+main = Blueprint('main', __name__, template_folder='templates')
+
+@main.route('/')
+def main_route():
+    return render_template("index.html")
+	
+@main.route('/hello')
+def hello():
+	cur = mysql.connection.cursor()
+	cur.execute('''SELECT * FROM discussion1.messages''')
+	msgs = cur.fetchall()
+	output = "<br>".join("Message#{0}:{1}".format(msgs.index(msg), msg[0]) for msg in msgs)
+	return output
